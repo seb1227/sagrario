956e965d0f1a03810fe3a6f5f124dfc800e9ba7f
seb1227 <36548972+seb1227@users.noreply.github.com>
2026-07-04 14:51:52 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='sagrario-v2';
+var CACHE='sagrario-v3';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));

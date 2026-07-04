1f8b53405f9725c9e2d20c2cb840957a632c6557
seb1227 <36548972+seb1227@users.noreply.github.com>
2026-07-04 14:05:34 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+var CACHE='sagrario-v1';
+var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(ks){
+    return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }).then(function(){return self.clients.claim();}));
+});
+self.addEventListener('fetch',function(e){
+  e.respondWith(caches.match(e.request).then(function(r){
+    return r || fetch(e.request).then(function(resp){
+      var cp=resp.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, cp); });
+      return resp;
+    }).catch(function(){ return caches.match('./index.html'); });
+  }));
+});
